44f44a390bbe72f1ad65aa74036a912b03809bf7
ashleydavis <ashley@codecapers.com.au>
2014-08-15 16:19:34 +1000

    Committed code 'forked' from CodePlex.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ TraceSpy
 ========
 
 TraceSpy is a pure .NET alternative to the very popular SysInternals DebugView tool.
+
+This is 'forked' from CodePlex.
+
+https://tracespy.codeplex.com
+
+
+The licence is Microsoft Public License (Ms-PL):
+
+https://tracespy.codeplex.com/license
---
+++ TraceSpyService/EtwRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TraceSpyService
+{
+    [DataContract]
+    public class EtwRecord
+    {
+        [DataMember]
+        public long Index { get; set; }
+
+        [DataMember]
+        public long Ticks { get; set; }
+
+        [DataMember]
+        public string ProcessName { get; set; }
+
+        [DataMember]
+        public int ProcessId { get; set; }
+
+        [DataMember]
+        public string Text { get; set; }
+    }
+}
